9831454a6b6d2ac127dd8e9d03ed07e832bcdb43
Abhijeetsingh <abhijeetsingh@MacBook-Air-3.local>
2023-10-11 07:22:17 +0530

    feat: Added a popup html page that is viewed when extension is clicked and removed some bugs

--- sandbox/youtube-abhi/content.js
+++ sandbox/youtube-abhi/content.js
@@ -1,34 +1,50 @@
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.action === 'autoScroll') {
+        const integerArg = message.integerArg; // Extract the arguments from the message
+        const videoListObj = message.videoListObj;
+        autoScroll(100);
+    }
+});
+
 async function autoScroll(numTitles) {
-  const scrollInterval = 1000; // Time interval between scroll actions in milliseconds
-  const numTitlesPerScroll = 20; // Number of video titles loaded per scroll (adjust as needed)
-  
-  const videoTitles = [];
+    const scrollInterval = 1000; // Time interval between scroll actions in milliseconds
+    const numTitlesPerScroll = 20; // Number of video titles loaded per scroll (adjust as needed)
 
-  while (videoTitles.length < numTitles) {
-      const videoTitleElements = document.querySelectorAll('#video-title.yt-simple-endpoint.style-scope.ytd-video-renderer');
-      
-      if (videoTitleElements.length === 0) {
-          // No more titles found, exit the loop
-          break;
-      }
-      index=0
-      for (const element of videoTitleElements) {
-          const titleText = element.innerText.trim();
-          if (titleText) {
-              videoTitles.push(i + ")))" +titleText);
-              index+=1;
-          }
-      }
+    const videoTitles = [];
 
-      // Scroll down by a certain amount (adjust as needed)
-      window.scrollBy(0, window.innerHeight * numTitlesPerScroll);
+    index = 0
+    while (videoTitles.length < numTitles) {
+        const videoTitleElements = document.querySelectorAll('#video-title.yt-simple-endpoint.style-scope.ytd-video-renderer');
 
-      // Wait for a brief moment to allow content to load
-      await new Promise(resolve => setTimeout(resolve, scrollInterval));
-  }
+        if (videoTitleElements.length === 0) {
+            // No more titles found, exit the loop
+            break;
+        }
+        // var videoList = document.getElementById('video-list');
+        // if (videoList) {
+        for (const element of videoTitleElements) {
+            const titleText = element.innerText.trim();
+            if (titleText) {
+                videoTitles.push(index + "\t\]\t" + titleText)
+                // console.log(index + "\t\]\t" + titleText)
+                // var listItem = document.createElement('li');
+                // listItem.textContent = index + '\]\t' + titleText;
+                // console.log(titleText)
+                // console.log(videoList)
+                // videoList.appendChild(listItem.appendChild(document.createTextNode(listItem)));
+                index += 1;
+            }
+        }
+        // } else {
+        // console.error('#video-list element not found');
+        // }
 
-  console.log(videoTitles.slice(0,numTitles).join('\n'));
+        // Scroll down by a certain amount (adjust as needed)
+        window.scrollBy(0, window.innerHeight * numTitlesPerScroll);
+
+        // Wait for a brief moment to allow content to load
+        await new Promise(resolve => setTimeout(resolve, scrollInterval));
+    }
+
+    console.log(videoTitles.slice(0, numTitles).join('\n'));
 }
-
-const numTitles = parseInt(prompt("Enter number"));
-autoScroll(numTitles);
