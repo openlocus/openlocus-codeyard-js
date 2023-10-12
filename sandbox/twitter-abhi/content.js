7b53c4c23693e79d47b55555d8d3282ed9622038
Abhijeetsingh <abhijeetsingh@MacBook-Air-3.local>
2023-10-12 08:43:45 +0530

    feat: Added a popup html page that is viewed when extension is clicked and removed some bugs

--- sandbox/twitter-abhi/content.js
+++ sandbox/twitter-abhi/content.js
@@ -1,12 +1,20 @@
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.action === 'autoScroll') {
+        const integerArg = message.integerArg; // Extract the arguments from the message
+        const videoListObj = message.videoListObj;
+        autoScroll(100);
+    }
+});
+
 async function autoScroll(numTitles) {
     const scrollInterval = 1000; // Time interval between scroll actions in milliseconds
     const numTitlesPerScroll = 20; // Number of tweet titles loaded per scroll (adjust as needed)
-    
+
     const tweetTitles = [];
-    i=1;
+    index = 0;
     while (tweetTitles.length < numTitles) {
         const tweetTitleElements = document.querySelectorAll('[data-testid="tweetText"]');
-        
+
         if (tweetTitleElements.length === 0) {
             // No more titles found, exit the loop
             break;
@@ -14,20 +22,17 @@ async function autoScroll(numTitles) {
         for (const element of tweetTitleElements) {
             const titleText = element.innerText.trim();
             if (titleText) {
-                tweetTitles.push(i + ")))" +titleText);
-                i+=1;
+                tweetTitles.push(index + "\t\]\t"+ titleText);
+                index += 1;
             }
         }
-  
+
         // Scroll down by a certain amount (adjust as needed)
         window.scrollBy(0, window.innerHeight * numTitlesPerScroll);
-  
+
         // Wait for a brief moment to allow content to load
         await new Promise(resolve => setTimeout(resolve, scrollInterval));
     }
-  
+
     console.log(tweetTitles.slice(0, numTitles).join('\n'));
-  }
-  
-  const numTitles = parseInt(prompt("Enter number"));
-  autoScroll(numTitles);
+}
